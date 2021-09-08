0871acedd321bcd48f551feae3336be6af14127f
alexDy1 <kratos19982571@gmail.com>
2021-09-08 09:16:43 -0400

    mcd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("METODO CONVENCIAONAL: " + Factorial.factorialSinRecursividad(5));
+        //System.out.println("METODO RECURSIVO: " +Factorial.factorial(5));
+
+        System.out.println(Factorial.mcd(412,184));
+    }
+
+}
